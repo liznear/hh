abe5260b2f8632f965deb809e4ecc58ad8b39ad2
Liiiz <liizznear@gmail.com>
2026-03-20 00:08:01 +0800

    fix(test): fix openai provider test

--- provider/openai_test.go
+++ provider/openai_test.go
@@ -2,10 +2,8 @@ package provider
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/liznear/hh/agent"
 )
@@ -13,6 +11,7 @@ import (
 func Test_OpenAIChatCompletionStream(t *testing.T) {
 	p := NewOpenAICompatibleProvider("http://localhost:8317/v1", os.Getenv("LOCAL_PROXY_API_KEY"))
 	ch, err := p.ChatCompletionStream(context.Background(), agent.ProviderRequest{
+		Model: "glm-5",
 		Messages: []agent.Message{
 			{
 				Role:    agent.RoleSystem,
@@ -27,9 +26,8 @@ func Test_OpenAIChatCompletionStream(t *testing.T) {
 	if err != nil {
 		t.Fatalf("fail to call ChatCompletionStream: %v", err)
 	}
-	time.Sleep(10 * time.Second)
-	for resp := range ch {
-		fmt.Printf("%v", resp)
+	for range ch {
+		return
 	}
 	t.Fail()
 }
